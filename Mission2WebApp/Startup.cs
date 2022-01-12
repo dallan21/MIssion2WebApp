94a7489ac46826abf3332f1b245509bdcf81d111
dallan21 <90338790+dallan21@users.noreply.github.com>
2022-01-12 10:47:46 -0700

    commit

--- Mission2WebApp/Startup.cs
+++ Mission2WebApp/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Mission2WebApp
 {
@@ -10,13 +11,30 @@ namespace Mission2WebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsEnvironment("Development"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default" ,
+                    pattern: "{controller-blah}/{action=Index}/{id?}"
+                    );
+            });
+
+
         }
     }
 }
