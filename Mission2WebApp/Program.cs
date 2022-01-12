666c4c72d4be8d5716fd2b3b45b9fbe0e8d05f18
dallan21 <90338790+dallan21@users.noreply.github.com>
2022-01-12 11:01:59 -0700

    Mission2 Commit

--- Mission2WebApp/Program.cs
+++ Mission2WebApp/Program.cs
@@ -11,7 +11,10 @@ namespace Mission2WebApp
 {
     public class Program
     {
-        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
