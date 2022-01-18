276b7b0d69cb790aa56d83845f7845ce6f07fa4d
dallan21 <90338790+dallan21@users.noreply.github.com>
2022-01-18 16:01:05 -0700

    Mission 3 commit

--- Mission2WebApp/Contorllers/blahcontroller.cs
+++ Mission2WebApp/Contorllers/blahcontroller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Mission2WebApp.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,6 +13,18 @@ namespace Mission2WebApp.Contorllers
     {
         // GET: /<controller>/
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Grade_Calc()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Grade_Calc(Grade_CalcModel model)
         {
             return View();
         }
---
+++ Mission2WebApp/Models/Grade_CalcModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission2WebApp.Models
+{
+    public class Grade_CalcModel
+    {
+        [Required]
+        //sets the validation so that it requires a number between 0 , 100
+        [Range(0,100)]
+        //This creates a getter and setter for you 
+        public byte assignment { get; set; }
+
+
+        //Below I am writing out the getter and setter just so I know what they are and how they work
+
+        //public void setassignment(byte input)
+        //{
+        //    this.assignment = input;
+        //}
+
+        //public byte getassignment ()
+        //{
+        //    return this.assignment;
+        //}
+        [Required]
+        [Range(0, 100)] 
+        public byte group_project { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public byte quiz { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public byte exam { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public byte intex { get; set; }
+
+    }
+}
